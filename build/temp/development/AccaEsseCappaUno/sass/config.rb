5c8ce21f6a24e4d65c6565afc24304b4650e0c84
nik <nicola.farina64@gmail.com>
2018-01-07 20:43:08 +0100

    Fist commit: Sencha cmd generated project

---
+++ build/temp/development/AccaEsseCappaUno/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = 'C:\Users\nik\Development\Javascript\ExtJs\accaessecappauno/build/.sass-cache'
---
+++ build/temp/development/AccaEsseCappaUno/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require C:\Users\nik\Development\Javascript\ExtJs\accaessecappauno\app.js
